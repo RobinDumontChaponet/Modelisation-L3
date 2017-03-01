298a153a9949375bca07520e4c3794a3ef134d8d
RobinDumontChaponet <robin@dumontchapo.net>
2017-03-01 08:14:17 +0100

    Initial Commit

---
+++ rom/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rom
+//
+//  Created by Robin Dumont-Chaponet on 01/03/2017.
+//  Copyright © 2017 Robin Dumont-Chaponet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
