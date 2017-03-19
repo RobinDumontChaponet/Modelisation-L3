4d9686be4bd44232882777a58839a65686df3e4d
RobinDumontChaponet <robin@dumontchapo.net>
2017-03-19 18:22:24 +0100

    CL Arguments

--- src/main.cpp
+++ src/main.cpp
@@ -23,64 +23,199 @@
 #include "algo/dfsNum.hpp"
 #include "algo/pcc.hpp"
 
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <iomanip>
 
 using namespace std;
 
+template<typename T> void printLeft(T t) {
+	cout << left << setw(22) << setfill(' ') << t;
+}
+
+/* Flag set by ‘--verbose’. */
+static int verbose_flag = 1;
+
 int main(int argc, char *argv[]) {
-	string fileName;
+	string inputFileName, outputFileName;
 
-	if (argc < 3) {
-		cerr << "Format: prog <option> <file> avec option: " << endl;
-		cerr << "\t-dfs : pour réaliser un DFS" << endl;
-		cerr << "\t-num : pour numéroter les sommets" << endl;
-		cerr << "\t-circle : detection de circuit" << endl;
-		cerr << "\t-topo : tri topologique" << endl;
-		cerr << "\t-connex : " << endl;
-		cerr << "\t..." << endl;
-		exit(1);
-	} 
+	static int query;
 
-	fileName = argv[argc-1];
+//	bool dFlag, nFlag, cFlag, tFlag, xFlag = false;
+
+	/* use gnu getOpt for CLI arguments */
+	int c;
+
+	while (1) {
+		static struct option long_options[] = {
+			/* These options set a flag. */
+			{"verbose", no_argument,       &verbose_flag, 1},
+			{"brief",   no_argument,       &verbose_flag, 0},
+
+			{"help",    no_argument,       0, 'h'},
+
+			{"dfs",   no_argument,       &query, 'd'},
+			{"num",     no_argument,       &query, 'n'},
+			{"pcc",     no_argument,       &query, 'p'},
+			{"circuit",  no_argument,       &query, 'c'},
+			{"topo",    no_argument,       &query, 't'},
+			{"connex",  no_argument,       &query, 'x'},
+
+			{"input",   required_argument, 0, 'i'},
+			{"output",  required_argument, 0, 'o'},
+			{0, 0, 0, 0}
+		};
+		/* getopt_long stores the option index here. */
+		int option_index = 0;
+
+		c = getopt_long (argc, argv, "hdnpctxi:o:", long_options, &option_index);
+
+		/* Detect the end of the options. */
+		if (c == -1)
+			break;
+
+		switch (c) {
+			case 0:
+				/* If this option set a flag, do nothing else now. */
+				if (long_options[option_index].flag != 0)
+					break;
+				printf ("option %s", long_options[option_index].name);
+				if (optarg)
+					printf (" with arg %s", optarg);
+				printf ("\n");
+				break;
+
+			case 'i':
+				inputFileName = optarg;
+				break;
+
+			case 'o':
+				outputFileName = optarg;
+				break;
+
+			case '?': case 'h':
+				cout << endl << "Usage: [option]…" << endl << endl;
+
+				printLeft("Option");
+				printLeft("GNU long option");
+				printLeft("Meaning");
+				cout << endl;
+
+				printLeft("-d");
+				printLeft("--dfs");
+				printLeft("réalise un DFS");
+				cout << endl;
+
+				printLeft("-p");
+				printLeft("--pcc");
+				printLeft("réalise une recherche du plus court chemin");
+				cout << endl;
+
+				printLeft("-n");
+				printLeft("--num");
+				printLeft("numérote les sommets");
+				cout << endl;
+
+				printLeft("-c");
+				printLeft("--circuit");
+				printLeft("réalise une detection de circuit");
+				cout << endl;
+
+				printLeft("-t");
+				printLeft("--topo");
+				printLeft("réalise un tri topologique");
+				cout << endl;
+
+				printLeft("-x");
+				printLeft("--connex");
+				printLeft("réalise une detection de connexité");
+				cout << endl;
+
+				printLeft("-i <fileName>");
+				printLeft("--input=<fileName>");
+				printLeft("fichier source");
+				cout << endl;
+
+				printLeft("-o <fileName>");
+				printLeft("--output=<fileName>");
+				printLeft("sauvegarde dans le fichier");
+				cout << endl;
+
+				exit( EXIT_FAILURE );
+
+				break;
+
+			default:
+				abort ();
+		}
+	}
+
+
+
+	if(inputFileName.empty()) {
+		cerr << "Aucun fichier fourni." << endl;
+		cerr << "See -h or --help for information" << endl;
+
+		exit(EXIT_FAILURE);
+	}
 
 	Parser<InfoArete, InfoSommet> parser;
 
-	if(!parser.parse(fileName)) {
+	if(!parser.parse(inputFileName)) {
 		cerr << "La lecture du fichier à échoué." << endl;
-		exit(1);
-	}	
-
+		exit(EXIT_FAILURE);
+	}
+	
 	const Graphe<InfoArete, InfoSommet> & graph = parser.getGraph();
 	string sourceName = parser.getSourceName();
 	string pitName = parser.getPitName();
 	string graphName = parser.getGraphName();
-	Algo* algo;
 
-	if (string(argv[1]).compare("-dfs") == 0) {
-		cout << "Execution de l'algorithme de recherche en profondeur d'abord (DFS)" << endl;
-		algo = new dfs(&graph);
-	}else if (string(argv[1]).compare("-num") == 0 ) {
-		cout << "Execution de l'algorithme de numérotation du graphe" << endl;
-		algo = new dfsNum(&graph);
-	}else if(string(argv[1]).compare("-pcc") == 0) {
-		if (argc != 5) {
-			cerr << "Format: prog -pcc <source> <destination> <file>" << endl;
-		}else
-		cout << "Execution de l'algorithme de recherche du plus court chemin" << endl;
-		string sourceName(argv[2]);
-		string destName(argv[3]);
-		algo = new PCC(&graph, sourceName, destName);
-	}else {
-		cerr << "algo " << argv[1] << " n'existe pas " << endl;
-		exit(1);
+	if(query) {
+		Algo* algo;
+
+		switch (query) {
+			case 'd':
+				cout << "Execution de l'algorithme de recherche en profondeur d'abord (DFS)" << endl;
+				algo = new dfs(&graph);
+			break;
+
+			case 'n':
+				cout << "Execution de l'algorithme de numérotation du graphe" << endl;
+				algo = new dfsNum(&graph);
+			break;
+
+			case 'p':
+					//		if (argc != 5) {
+					//			cerr << "Format: prog -pcc <source> <destination> <file>" << endl;
+					//		}else
+							cout << "Execution de l'algorithme de recherche du plus court chemin" << endl;
+					//		string sourceName(argv[2]);
+					//		string destName(argv[3]);
+					//		algo = new PCC(&graph, sourceName, destName);
+			break;
+
+			default:
+				break;
+		}
+
+		algo->evaluate();
+
+
+		if(verbose_flag)
+			cout << endl << "Aaaaaffichage" << endl;
+		// @TODO afficher
 	}
 
-	algo->evaluate();
-	//TODO afficher
+	if(!outputFileName.empty()) { // SI on choisit d'enregistrer
+		cout << "Sauvegarde dans le fichier " << '"' << outputFileName << '"';
+		if(parser.save(outputFileName))
+			cout << " réussie." << endl;
+		else
+			cout << " échouée." << endl;
+	}
 
-
-	// SI on choisit d'enregistrer
-	parser.save("../../../../../graphes/saved.gpr");
 
 	// Ici on a l'ancienne recherche du plus court chemin. Ça peut servir.r
 
@@ -173,7 +308,7 @@ int main(int argc, char *argv[]) {
 //	end = clock();
 //	cout << (double)(end-start)/CLOCKS_PER_SEC << " secondes." << endl << endl;
 
-	system("pause"); // cin.ignore().get();
+//	system("pause"); // cin.ignore().get();
 
 	return 0;
 }
