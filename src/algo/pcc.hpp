85e2087af4ee7b3ae27068e34459676bb3275ae9
dederobert <adinquer@yahoo.com>
2017-03-19 21:28:33 +0100

    correction pcc

--- src/algo/pcc.hpp
+++ src/algo/pcc.hpp
@@ -2,6 +2,7 @@
 #include "algo.hpp"
 #include "../Graphe.hpp"
 #include "../Error.hpp"
+#include "../PElement.hpp"
 #include <limits>
 
 class PCC:
@@ -19,13 +20,18 @@ class PCC:
 public:
 	PCC(const Graphe<InfoArete,InfoSommet>* graphe, const string& sourceName, const string& destName)
 	:Algo(graphe),k(0),n(_graphe->nombreSommets()),fin(false),S(NULL){
+		bool tmp1 = false, tmp2 = false;
 		PElement< Sommet<InfoSommet>>* l;
-		for(l=Algo::_graphe.lSommets; l; l=l->s)
-			if (l->v->v.nom.compare(sourceName) == 0)
-				dep = l->v;
-			else if(l->v->v.nom.compare(destName) == 0)
-				arriv = l->v;
-		if (dep == NULL || arriv == NULL) throw Error("PCC: sommet source ou sommet destination non trouvés");
+		for(l=Algo::_graphe->lSommets; l; l=l->s)
+			if (l->v->v.nom.compare(sourceName) == 0) {
+				dep = *l->v;
+				tmp1 = true;
+			}
+			else if(l->v->v.nom.compare(destName) == 0) {
+				arriv = *l->v;
+				tmp2 = true;
+			}
+		if (!tmp1 || !tmp2) throw Error("PCC: sommet source ou sommet destination non trouvés");
 	}
 	~PCC();
 	Graphe<InfoArete,InfoSommet> evaluate();
@@ -37,9 +43,9 @@ Graphe<InfoArete, InfoSommet> PCC::evaluate() {
 	PElement<Sommet<InfoSommet>>* l;
 	
 	//Met tout les sommets à +infiny
-	for (l=Algo::_graphe.lSommets; l; l=l->s)
-		l->v->lambda = std::numeric_limits<int>::max();
-	dep.v->lambda = 0;
+	for (l=Algo::_graphe->lSommets; l; l=l->s)
+		l->v->v.lambda = std::numeric_limits<int>::max();
+	dep.v.lambda = 0;
 
 	while (k<=n-1 && !fin) {
 		k++;
@@ -47,7 +53,7 @@ Graphe<InfoArete, InfoSommet> PCC::evaluate() {
 		PElement<Sommet<InfoSommet>> * mTmp ;
 		for (mTmp = S; mTmp; mTmp = mTmp->s) {
 			PElement<pair<Sommet<InfoSommet> *, Arete<InfoArete, InfoSommet>* >> * sTmp;
-			for (sTmp = graph.successeurs(mTmp->v); sTmp; sTmp = sTmp->s) {
+			for (sTmp = Algo::_graphe->successeurs(mTmp->v); sTmp; sTmp = sTmp->s) {
 				pair<Sommet<InfoSommet> *, Arete<InfoArete, InfoSommet>*> xj = *sTmp->v;
 
 				double lSucc = xj.second->debut->v.lambda + xj.second->v.cout;
@@ -55,12 +61,12 @@ Graphe<InfoArete, InfoSommet> PCC::evaluate() {
 					xj.first->v.lambda = lSucc;
 
 					Mp = new PElement<Sommet<InfoSommet>> (xj.first, Mp);
-					pj [xj.first->clef] = *xj.second->debut;
 				}
 			}
 		}
 		S = Mp;
 	}
 
-	return S;
+	//Il faut retourner autre chose car la solution est dans M et on doit retourner un graphe
+	return _res;
 }
